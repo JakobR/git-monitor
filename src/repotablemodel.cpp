d94c517f9f30b6332df2b245180492260fec0ed2
Jakob Rath <git@jakobrath.eu>
2025-04-18 14:04:23 +0200

    Connect RepoTableModel with RepoManager

--- src/repotablemodel.cpp
+++ src/repotablemodel.cpp
@@ -1,22 +1,36 @@
 #include "repotablemodel.h"
 #include <QSize>
 
-namespace {
-    namespace Column {
-        enum Column : int {
-            Path = 0,
-            Status = 1,
-            COUNT = 2,
-        };
-    }
-}
-
-RepoTableModel::RepoTableModel(QObject *parent)
+RepoTableModel::RepoTableModel(QObject* parent)
     : QAbstractTableModel(parent)
 {}
 
+void RepoTableModel::setRepoManager(RepoManager* newRepoManager)
+{
+    if (m_repoManager)
+        disconnect(m_repoManager, &RepoManager::repoChanged, this, &RepoTableModel::on_repo_changed);
+
+    beginResetModel();
+    m_repoManager = newRepoManager;
+    endResetModel();
+
+    if (m_repoManager)
+        connect(m_repoManager, &RepoManager::repoChanged, this, &RepoTableModel::on_repo_changed);
+}
+
+void RepoTableModel::addRepo(RepoSettings settings)
+{
+    Q_ASSERT(m_repoManager);
+    beginInsertRows(QModelIndex(), rowCount(), rowCount());
+    m_repoManager->addNewRepo(std::move(settings));
+    endInsertRows();
+}
+
 QVariant RepoTableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+    if (orientation != Qt::Horizontal)
+        return QVariant();
+
     if (role == Qt::DisplayRole) {
         switch (section) {
         case Column::Path:
@@ -29,16 +43,7 @@ QVariant RepoTableModel::headerData(int section, Qt::Orientation orientation, in
     return QVariant();
 }
 
-int RepoTableModel::rowCount(const QModelIndex &parent) const
-{
-    if (parent.isValid())
-        return 0;
-
-    // FIXME: Implement me!
-    return 0;
-}
-
-int RepoTableModel::columnCount(const QModelIndex &parent) const
+int RepoTableModel::columnCount(QModelIndex const& parent) const
 {
     if (parent.isValid())
         return 0;
@@ -46,11 +51,53 @@ int RepoTableModel::columnCount(const QModelIndex &parent) const
     return Column::COUNT;
 }
 
-QVariant RepoTableModel::data(const QModelIndex &index, int role) const
+int RepoTableModel::rowCount(QModelIndex const& parent) const
 {
+    if (!m_repoManager)
+        return 0;
+    if (parent.isValid())
+        return 0;
+    return m_repoManager->repos().size();
+}
+
+QVariant RepoTableModel::data(QModelIndex const& index, int role) const
+{
+    Q_ASSERT(m_repoManager);
+    Q_ASSERT(index.isValid());
     if (!index.isValid())
         return QVariant();
 
-    // FIXME: Implement me!
+    if (role == Qt::DisplayRole) {
+        auto const& repo = m_repoManager->repos().at(index.row());
+
+        switch (index.column()) {
+        case Column::Path:
+            return repo->settings().path;
+        case Column::Status:
+            if (repo->activity() == RepoActivity::Checking)
+                return tr("Checking...");
+            switch (repo->status()) {
+                case RepoStatus::Ok:
+                    return tr("OK");
+                case RepoStatus::DirtyOrOutdated:
+                    return tr("Dirty");
+                case RepoStatus::Unknown:
+                    return tr("???");
+                case RepoStatus::Error:
+                    return tr("Error");
+            }
+        }
+    }
+
     return QVariant();
+}
+
+void RepoTableModel::on_repo_changed(Repo* repo)
+{
+    if (sender() != m_repoManager)
+        return;
+    auto const row = repo->index();
+    auto const topLeft = index(row, Column::MIN);
+    auto const bottomRight = index(row, Column::MAX);
+    emit dataChanged(topLeft, bottomRight);
 }
--- src/repotablemodel.h
+++ src/repotablemodel.h
@@ -1,6 +1,7 @@
 #ifndef REPOTABLEMODEL_H
 #define REPOTABLEMODEL_H
 
+#include "repomanager.h"
 #include <QAbstractTableModel>
 
 class RepoTableModel : public QAbstractTableModel
@@ -8,16 +9,35 @@ class RepoTableModel : public QAbstractTableModel
     Q_OBJECT
 
 public:
-    explicit RepoTableModel(QObject *parent = nullptr);
+    explicit RepoTableModel(QObject* parent = nullptr);
+
+    // RepoManager* repoManager() const { return m_repoManager; }
+    void setRepoManager(RepoManager* repoManager);
+
+    void addRepo(RepoSettings settings);
 
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    int rowCount(QModelIndex const& parent = QModelIndex()) const override;
+    int columnCount(QModelIndex const& parent = QModelIndex()) const override;
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant data(QModelIndex const& index, int role = Qt::DisplayRole) const override;
+
+    struct Column {
+        enum ColumnIndices : int {
+            Path = 0,
+            Status = 1,
+            COUNT = 2,
+        };
+        inline static constexpr int MIN = 0;
+        inline static constexpr int MAX = COUNT - 1;
+    };
+
+private slots:
+    void on_repo_changed(Repo* repo);
 
 private:
+    RepoManager* m_repoManager = nullptr;
 };
 
 #endif // REPOTABLEMODEL_H
