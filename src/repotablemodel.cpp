e389d08b65c320c4425c4745f0fa816d53d01b4f
Jakob Rath <git@jakobrath.eu>
2025-04-19 09:46:01 +0200

    Display more statistics in repo table

--- src/repotablemodel.cpp
+++ src/repotablemodel.cpp
@@ -37,8 +37,18 @@ QVariant RepoTableModel::headerData(int section, Qt::Orientation orientation, in
             return tr("Repository Path");
         case Column::Status:
             return tr("Status");
+        case Column::Uncommitted:
+            return tr("Uncommitted");
+        case Column::HEAD:
+            return tr("HEAD");
+        case Column::Branches:
+            return tr("All Branches");
+        case Column::Remote:
+            return tr("Remote Branches");
         }
     }
+
+    // TODO: implement Qt::ToolTipRole with explanations
 
     return QVariant();
 }
@@ -68,28 +78,115 @@ QVariant RepoTableModel::data(QModelIndex const& index, int role) const
         return QVariant();
 
     if (role == Qt::DisplayRole) {
-        auto const& repo = m_repoManager->repos().at(index.row());
+        Repo const* repo = m_repoManager->repos().at(index.row());
 
         switch (index.column()) {
         case Column::Path:
-            return repo->settings().path;
+            return getPathData(repo);
         case Column::Status:
-            if (repo->activity() == RepoActivity::Checking)
-                return tr("Checking...");
-            switch (repo->status()) {
-                case RepoStatus::Ok:
-                    return tr("OK");
-                case RepoStatus::DirtyOrOutdated:
-                    return tr("Dirty");
-                case RepoStatus::Unknown:
-                    return tr("???");
-                case RepoStatus::Error:
-                    return tr("Error");
-            }
+            return getStatusData(repo, role);
+        case Column::Uncommitted:
+            return getUncommittedData(repo);
+        case Column::HEAD:
+            return getHEADData(repo);
+        case Column::Branches:
+            return getBranchesData(repo);
+        case Column::Remote:
+            return getRemoteData(repo);
         }
     }
 
+    // TODO: implement Qt::ToolTipRole with more detailed messages
+
     return QVariant();
+}
+
+QVariant RepoTableModel::getPathData(Repo const* repo) const
+{
+    return repo->settings().path;
+}
+
+QVariant RepoTableModel::getStatusData(Repo const* repo, int role) const
+{
+    // TODO: red bold font on error
+    if (role != Qt::DisplayRole)
+        return QVariant();
+    if (repo->activity() == RepoActivity::Checking)
+        return tr("Checking...");
+    switch (repo->status()) {
+        case RepoStatus::Ok:
+            return tr("OK");
+        case RepoStatus::DirtyOrOutdated:
+            return tr("Dirty");
+        case RepoStatus::Unknown:
+            return tr("???");
+        case RepoStatus::Error:
+            return tr("Error");
+    }
+    return QVariant();
+}
+
+QVariant RepoTableModel::getUncommittedData(Repo const* repo) const
+{
+    auto const& uncommitted = repo->statistics().uncommitted;
+    if (!uncommitted)
+        return QVariant();
+    return tr("%1 changes").arg(*uncommitted);
+}
+
+QVariant RepoTableModel::getHEADData(Repo const* repo) const
+{
+    auto const& head_ab = repo->statistics().head_ahead_behind;
+    auto const& head_state = repo->statistics().head_state;
+    // qDebug() << "repo " << repo->settings().path << "HEAD state:" << (int)head_state << " head_ab:" << head_ab.has_value();
+    // if (!head_ab && head_state == git::branch_state::unknown)
+    //     return QVariant();
+    // if (!head_ab && head_state == git::branch_state::connection_error)
+    //     return QVariant();
+    QString result;
+    if (head_ab && head_ab->ahead > 0)
+        result += tr("%1 ahead").arg(head_ab->ahead);
+    if (head_ab && head_ab->behind > 0) {
+        if (!result.isEmpty())
+            result += ", ";
+        result += tr("%1 behind").arg(head_ab->behind);
+    }
+    if (head_state == git::branch_state::outdated) {
+        if (!result.isEmpty())
+            result += ", ";
+        result += tr("outdated");
+    }
+    if (result.isEmpty())
+        return tr("OK");
+    return result;
+}
+
+QVariant RepoTableModel::getBranchesData(Repo const* repo) const
+{
+    auto const& total_ab = repo->statistics().total_ahead_behind;
+    if (!total_ab)
+        return QVariant();
+    QString result;
+    if (total_ab->ahead > 0)
+        result += tr("%1 ahead").arg(total_ab->ahead);
+    if (total_ab->behind > 0) {
+        if (!result.isEmpty())
+            result += ", ";
+        result += tr("%1 behind").arg(total_ab->behind);
+    }
+    if (result.isEmpty())
+        return tr("OK");
+    return result;
+}
+
+QVariant RepoTableModel::getRemoteData(Repo const* repo) const
+{
+    auto const& branches_outdated = repo->statistics().branches_outdated;
+    if (!branches_outdated)
+        return QVariant();
+    if (*branches_outdated == 0)
+        return tr("Up-to-date");
+    return tr("%1 outdated").arg(*branches_outdated);
 }
 
 void RepoTableModel::on_repo_changed(Repo* repo)
--- src/repotablemodel.h
+++ src/repotablemodel.h
@@ -26,12 +26,24 @@ public:
     struct Column {
         enum ColumnIndices : int {
             Path = 0,
-            Status = 1,
-            COUNT = 2,
+            Status,
+            Uncommitted,
+            HEAD,
+            Branches,
+            Remote,
+            COUNT,
         };
         inline static constexpr int MIN = 0;
         inline static constexpr int MAX = COUNT - 1;
     };
+
+private:
+    QVariant getPathData(Repo const* repo) const;
+    QVariant getStatusData(Repo const* repo, int role) const;
+    QVariant getUncommittedData(Repo const* repo) const;
+    QVariant getHEADData(Repo const* repo) const;
+    QVariant getBranchesData(Repo const* repo) const;
+    QVariant getRemoteData(Repo const* repo) const;
 
 private slots:
     void on_repo_changed(Repo* repo);
